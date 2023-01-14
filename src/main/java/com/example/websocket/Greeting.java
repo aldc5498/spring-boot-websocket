b9d902473f13eaa62eadece5784fd8b4327ada16
alish <aldc1233@gmail.com>
2023-01-14 14:41:44 +0545

    initial commit

---
+++ src/main/java/com/example/websocket/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private String message;
+
+}
